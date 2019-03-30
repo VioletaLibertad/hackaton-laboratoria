3554af3f4fb73159d598b8390d78a51eca7a9b3f
VioletaLibertad <morada.libertad@gmail.com>
2019-03-30 10:23:50 -0300

    first commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faIgloo)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Icon test <FontAwesomeIcon icon="igloo"/>
           </p>
           <a
             className="App-link"
