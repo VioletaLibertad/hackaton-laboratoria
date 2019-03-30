b948423bc86f347f5ecf871c01d551bc7c2adfd8
VioletaLibertad <morada.libertad@gmail.com>
2019-03-30 11:32:17 -0300

    rutas creadas

---
+++ src/Components/Community/Community.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Community extends Component {
+  render(){
+    return(
+      <p>Este es Community</p>
+    )
+  }
+}
+
+export default Community;
---
+++ src/Components/Discover/Discover.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Discover extends Component {
+  render(){
+    return(
+      <p>Este es Discover</p>
+    )
+  }
+}
+
+export default Discover;
