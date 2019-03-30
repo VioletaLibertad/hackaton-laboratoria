3e7fa1ce4333eab03b5cc32095ee45d3d9624a91
VioletaLibertad <morada.libertad@gmail.com>
2019-03-30 15:31:09 -0300

    login component added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import Logo from '../../img/logo.jpeg';
 
 class Login extends Component {
   render(){
     return(
-      <p>Este es Login</p>
+      <div>
+        <img src={Logo} alt="logo"/>
+        <input type="text"/>
+        <input type="password"/>
+        <Link className="login-button" to="/descubre">Ingresar</Link>
+      </div>
     )
   }
 }
